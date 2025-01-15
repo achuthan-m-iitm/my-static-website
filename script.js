2d5c135ba1e69a8d6af854f933beeed558c07bc8
achuthan-m-iitm <22f3002867@ds.study.iitm.ac.in>
2025-01-15 10:21:41 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+function displayMessage() {
+    const messageParagraph = document.getElementById('message');
+    messageParagraph.textContent = 'Hello! Thanks for clicking the button!';
+}
